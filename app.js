e92a643d343757ba6957d060b92a5df37a1e9ae5
Mahmud Qosim <qmahmud222@gmail.com>
2022-12-08 23:58:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const errorMiddleware = require("./middlewares/errors")
 
 const app = express()
 
-app.use(cors({origin: https://learnoh-client.vercel.app}))
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
